30e44777bb2f03ee01c74afb7c97fd69a621945e
vitordbo <vitorduarte177@gmail.com>
2022-07-14 14:53:58 -0300

    syntax

---
+++ Syntax.php
@@ -0,0 +1,23 @@
+<?php
+// PHP code goes here
+
+//Basic PHP Syntax
+//A PHP script can be placed anywhere in the document.
+// PHP statements end with a ;
+
+//In PHP, keywords are not case-sensitive butH all variable names are case-sensitive!
+//A PHP script starts with <?php and ends with ?>:
+
+<!-- could be inside the html -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
