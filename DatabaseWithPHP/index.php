deea8633199ce4c533332f75edd58a7c945eda1e
vitordbo <vitorduarte177@gmail.com>
2022-08-15 15:30:55 -0300

    update

--- DatabaseWithPHP/index.php
+++ DatabaseWithPHP/index.php
@@ -21,14 +21,25 @@
                 text-align:center;
                 border: 1px solid #ccc;
             }
+            .hide{
+                display: none;
+            }
         </style>
     </head>
     <body>
         <h1>Inserting data using php</h1>
-        <form method="post">
+        <form id="save-form" method="post">
             <input type="text" name="name" placeholder="Type your name" required>
             <input type="email" name="email" placeholder="Type your email" required>
             <button type="submit" name="save">Save</button>
+        </form> 
+
+        <form class="hide" id="update-form" method="post">
+            <input type="hidden" id="update_id" name="update_id" placeholder="Update your id" required>
+            <input type="text" id="update_name" name="update_name" placeholder="Update your name" required>
+            <input type="email" id="update_email" name="update_email" placeholder="Update your email" required>
+            <button type="submit" name="update">Update</button>
+            <button type="button" id="cancel" name="cancel">cancel</button>
         </form> 
 
         <?php  // INSERT DATA
@@ -85,6 +96,47 @@
         }
         ?>
 
+        <?php
+        // UPDATE FORM
+        if(isset($_POST['update']) && isset($_POST['update_id']) && isset($_POST['update_name']) && isset($_POST['update_email'])){
+            // UPDATE DATA 
+            $id = cleanPost($_POST['update_id']);
+            $nameUpdate = cleanPost($_POST['update_name']);
+            $emailUpdate = cleanPost($_POST['update_email']);
+        
+            
+             // check if is a empty name 
+             if ($nameUpdate == "" || $nameUpdate == null){
+                echo "Name can't be empty";
+                exit();
+            }
+
+            // check if is a empty email
+            if($emailUpdate == "" || $emailUpdate==null){
+                echo "Email can't be empty";
+                exit();    
+            }
+
+            // check if is a ok name 
+            if (!preg_match("/^[a-zA-Z-' ]*$/",$nameUpdate)) {
+                echo "<b style='color:red'>Only letters and spaces area allowed</b>";
+                exit();
+            }
+
+            //check if is a ok email 
+            if (!filter_var($emailUpdate, FILTER_VALIDATE_EMAIL)) {
+                echo "<b style='color:red'>This email isn't valid!</b>";
+                exit();
+            }
+
+            $sql = $pdo->prepare("UPDATE clients SET name=?, email=? WHERE id=?");
+            $sql->execute(array($nameUpdate, $emailUpdate, $id));
+
+            echo "Update ".$sql->rowCount()." line";
+            
+        }         
+        ?>
+
         <?php 
             // SELECT DATA FROM TABLE => can add a filter WHERE email ? 
             $sql = $pdo->prepare("SELECT * FROM clients");
@@ -111,6 +163,7 @@
                     <th>ID</th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Actions</th>
                 </tr>";
 
                 foreach($dataSelect as $key => $value){
@@ -118,6 +171,7 @@
                                 <th>".$value['id']."</th>
                                 <th>".$value['name']."</th>
                                 <th>".$value['email']."</th>
+                                <th><a href='#' class='btn-update' data-id='".$value['id']."' data-name='".$value['name']."' data-email='".$value['email']."'>Update</a></th> 
                             </tr>";
                 }
 
@@ -129,5 +183,26 @@
         ?>
 
         <br>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            // to update 
+            $(".btn-update").click(function(){
+                var id = $(this).attr('data-id');
+                var name = $(this).attr('data-name');
+                var email = $(this).attr('data-email');
+
+                $('#save-form').addClass('hide');
+                $('#update-form').removeClass('hide');
+
+                $("#update_id").val(id);
+                $("#update_name").val(name);
+                $("#update_email").val(email);  
+            });
+
+            $('#cancel').click(function(){
+                $('#save-form').removeClass('hide');
+                $('#update-form').addClass('hide');
+            });
+        </script>
     </body>
 </html>
