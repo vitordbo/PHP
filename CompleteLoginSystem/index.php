5751d6e0c751aafb2e700e1e190eadd04216c64c
vitordbo <vitorduarte177@gmail.com>
2022-08-19 18:19:44 -0300

    success alert

--- CompleteLoginSystem/index.php
+++ CompleteLoginSystem/index.php
@@ -11,18 +11,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
 <body>
     <form>
         <h1>Login</h1>
 
-        <?php if(isset($_GET['result'])){ ?>
-
+        <?php if(isset($_GET['result']) && ($_GET['result']=="ok")){ ?>
+            <div class="success animate__animated animate__rubberBand">
+                User registered successfully
+            </div>
         <?php }?>
 
-        <div class="success">
-        user registered successfully
-        </div>
 
         <div class="input-group">
             <img class="input-icon" src="img/mail.png">
@@ -39,4 +39,14 @@
 
     </form>
 </body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    <?php if(isset($_GET['result']) && ($_GET['result']=="ok")){ ?> 
+    <script>
+        // after 3 seconds the green alert of success will disappear
+        setTimeout(() => {
+            $('.success').addClass('hide');
+        }, 3000);
+    </script>
+    <?php }?>
 </html>
