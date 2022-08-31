0afcd9812970e418f225539066e850187732e97b
vitordbo <vitorduarte177@gmail.com>
2022-08-31 15:31:58 -0300

    databases

---
+++ CompleteLoginSystemOOP/class/config.php
@@ -0,0 +1,16 @@
+<?php
+
+//CONFIGURAÇÕES DO BANCO DE DADOS
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('SENHA','');
+define('BANCO','login');
+
+function limpaPost($dados){
+    $dados = trim($dados);
+    $dados = stripslashes($dados);
+    $dados = htmlspecialchars($dados);
+    return $dados;
+}
+
+?>
